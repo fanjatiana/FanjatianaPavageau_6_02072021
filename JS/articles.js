b54016e7059731c3925c4d30e55701eac537674f
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2021-07-06 15:32:06 +0200

    premier essai intégration de contenu js

--- JS/articles.js
+++ JS/articles.js
@@ -3,6 +3,7 @@ const article = document.querySelector('article');
 const idPhotograph = document.getElementById('id-photograph');
 const infosPhotograph = document.getElementById('infos-photograph');
 const tagLinks= document.getElementById('tag-links');
+const paragraph = document.getElementsByClassName('.paragraph');
 
 /*************************************************************************** */
 
@@ -13,12 +14,61 @@ const tagLinks= document.getElementById('tag-links');
 
 
 //chargement du fichier Json avec l'URL
+
+
+
 const getPictures = async () => {
     let response = await fetch('https://cors-anywhere.herokuapp.com/https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeData.json')
     let data = await response.json()
-    console.log(data)
+    console.log(data.photographers[0]);
+
+   
+    let photographersList = data.photographers;
+    let photographersMedia = data.media;
+    
+    for (let element of photographersList){
+  
+
+            article.innerHTML= 
+            "<h3>" + data.photographers[0]['name'] + "</h3>" 
+            + "<p>" + data.photographers[0]['id'] + "</p>" +
+            "<p>" + data.photographers[0]['city'] + "</p>" +
+            "<p>" + data.photographers[0]['country'] + "</p>" +
+            "<ul>" + "<li>" + "<a href='#'>" + "#" +data.photographers[0]['tags']+ "</a>" + "</li>" + "</ul>" ;
+           
+    
+
+    }
+    
+    
+
+    
+    
+    //const name = data['photographers'][0]['name'];
+
+    
+  
+  
+
+       
+   
+
+    
+    
+    /*for (let element of photographersList){
+       
+       article.innerHTML= "<h3>" + element.name + "</h3>" +
+       "<p>" + element.id + "</p>" +
+       "<p>" + element.city + "</p>" +
+       "<p>" + element.country + "</p>" +
+       "<ul>" + "<li>" + "<a href='#'>" + "#" +element.tags[0] + "</a>" + "</li>" + "</ul>" +
+       "<ul>" + "<li>" + "<a href='#'>" + "#" +element.tags[1] + "</a>" + "</li>" + "</ul>" ;
+    }*/
+
+    
+    
   }
-  getPictures()
+  getPictures();
 
 
 
