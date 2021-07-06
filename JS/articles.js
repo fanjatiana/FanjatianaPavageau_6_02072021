8266f362f42f97396c50caad0de63c25a60cb41f
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2021-07-06 23:27:13 +0200

    intégration contenu html de la page index avec javscript + début mise en page avec SASS

--- JS/articles.js
+++ JS/articles.js
@@ -4,6 +4,7 @@ const idPhotograph = document.getElementById('id-photograph');
 const infosPhotograph = document.getElementById('infos-photograph');
 const tagLinks= document.getElementById('tag-links');
 const paragraph = document.getElementsByClassName('.paragraph');
+const container = document.getElementById('container');
 
 /*************************************************************************** */
 
@@ -20,21 +21,50 @@ const paragraph = document.getElementsByClassName('.paragraph');
 const getPictures = async () => {
     let response = await fetch('https://cors-anywhere.herokuapp.com/https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeData.json')
     let data = await response.json()
-    console.log(data.photographers[0]);
+    console.log(data.photographers);
 
    
     let photographersList = data.photographers;
     let photographersMedia = data.media;
     
     for (let element of photographersList){
-  
+        let portrait = element.portrait;
+        let name = element.name;
+        let city = element.city;
+        let price = element.price;
+        let country = element.country;
+        let tagline = element.tagline;
+        let tags = element.tags;
+        
+        container.innerHTML += 
+        `<article>
+           <section id="id-photograph">
+               <a href="#">
+                   <img src="/Photos/Sample Photos/Photographers ID Photos/${portrait}">
+                   <h2>${name}</h2>
+               </a>
+           </section>
+           <section id="infos-photograph">
+               <p>${country}, ${city}</p>
+               <p>${tagline}</p>
+               <p>${price}€/jour</p>
+           </section>
+           <section id="tag-links">
+               <ul>
+                   <li><a href="#">#${tags}</a></li>
 
-            article.innerHTML= 
-            "<h3>" + data.photographers[0]['name'] + "</h3>" 
+               </ul>
+           </section>
+       </article>`;
+            
+           
+           
+           
+           /*"<h3>" + data.photographers[0]['name'] + "</h3>" 
             + "<p>" + data.photographers[0]['id'] + "</p>" +
             "<p>" + data.photographers[0]['city'] + "</p>" +
             "<p>" + data.photographers[0]['country'] + "</p>" +
-            "<ul>" + "<li>" + "<a href='#'>" + "#" +data.photographers[0]['tags']+ "</a>" + "</li>" + "</ul>" ;
+            "<ul>" + "<li>" + "<a href='#'>" + "#" +data.photographers[0]['tags']+ "</a>" + "</li>" + "</ul>" ;*/
            
     
 
