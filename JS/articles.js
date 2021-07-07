a970e3d6d80a51969d9c5da6041a36b1e877c86a
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2021-07-07 22:53:25 +0200

    ajout des liens tags des photographes avec javascript

--- JS/articles.js
+++ JS/articles.js
@@ -1,12 +1,9 @@
 /********************************* DOM ****************************************/
-const article = document.getElementById('id-photographers');
-const idPhotograph = document.getElementById('id-photograph');
-const infosPhotograph = document.getElementById('infos-photograph');
-const tagLinks= document.getElementById('tag-links');
-const paragraph = document.getElementsByClassName('.paragraph');
+const article = document.querySelector('article');
+const idPhotograph = document.getElementsByClassName('.id-photograph');
+const infosPhotograph = document.getElementsByClassName('.infos-photograph');
+const tagLinks = document.getElementsByClassName('.tag-links');
 const container = document.getElementById('container');
-const list = document.querySelector('.list');
-console.log(article)
 
 /*************************************************************************** */
 
@@ -16,21 +13,23 @@ console.log(article)
 
 
 
-//chargement du fichier Json avec l'URL
-
-
-
+/*chargement du fichier Json avec l'URL*/
 const getPictures = async () => {
     let response = await fetch('https://cors-anywhere.herokuapp.com/https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeData.json')
     let data = await response.json()
     console.log(data.photographers);
-   
 
-   
+    /* on cré une variable pour réccupérer la liste des photographes du json*/
     let photographersList = data.photographers;
-    let photographersMedia = data.media;
     
-    for (let element of photographersList){
+    
+    /*on cré et initialise une variable pour le nombre de liens tag des photographes*/
+    let numbTags = 0;
+    
+    /*boucle for pour ajouter les articles des photographes*/
+    for (let element of photographersList) {
+
+        /* création de variables pour recupérer les données json*/
         let portrait = element.portrait;
         let name = element.name;
         let city = element.city;
@@ -38,38 +37,38 @@ const getPictures = async () => {
         let country = element.country;
         let tagline = element.tagline;
         let tags = element.tags;
-        
-        article.innerHTML += 
-        `
-           <section id="id-photograph">
+
+        /*on incrémente la variable pour recupérer le nombre de tags des photographes*/
+        numbTags++;
+
+        /*on cré un bloc de code dans le html qui affichera les photos et infos des photographes*/
+        container.innerHTML +=
+            `<article>
+           <section class="id-photograph">
                <a href="#">
                    <img src="/Photos/gallery/Photographers-Photos/${portrait}">
                    <h2>${name}</h2>
                </a>
            </section>
-           <section id="infos-photograph">
+           <section class="infos-photograph">
                <p>${country}, ${city}</p>
                <p>${tagline}</p>
                <p>${price}€/jour</p>
            </section>
+           <section class="tag-links">
+               <ul id="tagList${numbTags}"></ul>
+           </section>
+       </article>`;
 
-       `;
-
-       for (var j = 0; j < tags.length; j++) {
-        var listTags = document.createElement('li');
-        listTags.textContent = tags[j];
-       article.appendChild(listTags);
-      }
-    
-    
+       /*on effectue une boucle forEach pour ajouter les liens des tags associés aux photographes*/
+        let tagList = document.getElementById("tagList" + numbTags);
+        [...tags].forEach(photographersTags => {
+            tagList.innerHTML +=
+                ` <li class = "tags"><a href="#">#${photographersTags}</a></li>`
+        })
     }
-    
-    
-
-    
-    
-  }
-  getPictures();
+}
+getPictures();
 
 
 
@@ -78,9 +77,4 @@ const getPictures = async () => {
 
 
 
-/*  <section id="tag-links">
-<ul>
-<li><a href="#">#${tags}</a></li>
 
-</ul>
-</section>*/
