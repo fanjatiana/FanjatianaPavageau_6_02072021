b103beb0db7febc3884b03093892eef597299886
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2021-07-11 16:37:47 +0200

    essais filtre javascript

--- JS/articles.js
+++ JS/articles.js
@@ -1,9 +1,7 @@
 /********************************* DOM ****************************************/
 const article = document.querySelector('article');
-const idPhotograph = document.getElementsByClassName('.id-photograph');
-const infosPhotograph = document.getElementsByClassName('.infos-photograph');
-const tagLinks = document.getElementsByClassName('.tag-links');
 const container = document.getElementById('container');
+
 
 /*************************************************************************** */
 
@@ -16,34 +14,37 @@ const container = document.getElementById('container');
 /*chargement du fichier Json avec l'URL*/
 const getPictures = async () => {
     let response = await fetch('https://cors-anywhere.herokuapp.com/https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeData.json')
-    let data = await response.json()
-    console.log(data.photographers);
+    let data = await response.json();
+
+
 
     /* on cré une variable pour réccupérer la liste des photographes du json*/
     let photographersList = data.photographers;
-    
-    
+
+
+
     /*on cré et initialise une variable pour le nombre de liens tag des photographes*/
     let numbTags = 0;
-    
-    /*boucle for pour ajouter les articles des photographes*/
-    for (let element of photographersList) {
 
-        /* création de variables pour recupérer les données json*/
-        let portrait = element.portrait;
-        let name = element.name;
-        let city = element.city;
-        let price = element.price;
-        let country = element.country;
-        let tagline = element.tagline;
-        let tags = element.tags;
+    function articles() {
+        /*boucle for pour ajouter les articles des photographes*/
+        for (let element of photographersList) {
 
-        /*on incrémente la variable pour recupérer le nombre de tags des photographes*/
-        numbTags++;
+            /* création de variables pour recupérer les données json*/
+            let portrait = element.portrait;
+            let name = element.name;
+            let city = element.city;
+            let price = element.price;
+            let country = element.country;
+            let tagline = element.tagline;
+            let tags = element.tags;
 
-        /*on cré un bloc de code dans le html qui affichera les photos et infos des photographes*/
-        container.innerHTML +=
-            `<article>
+            /*on incrémente la variable pour recupérer le nombre de tags des photographes*/
+            numbTags++;
+
+            /*on cré un bloc de code dans le html qui affichera les photos et infos des photographes*/
+            container.innerHTML +=
+                `<article>
            <section class="id-photograph">
                <a href="#">
                    <img src="/Photos/gallery/Photographers-Photos/${portrait}">
@@ -60,18 +61,61 @@ const getPictures = async () => {
            </section>
        </article>`;
 
-       /*on effectue une boucle forEach pour ajouter les liens des tags associés aux photographes*/
-        let tagList = document.getElementById("tagList" + numbTags);
-        [...tags].forEach(photographersTags => {
-            tagList.innerHTML +=
-                ` <li class = "tags"><a href="#">#${photographersTags}</a></li>`
-        })
+            /*on effectue une boucle forEach pour ajouter les liens des tags associés aux photographes*/
+            let tagList = document.getElementById("tagList" + numbTags);
+            [...tags].forEach(photographersTags => {
+                tagList.innerHTML +=
+                    ` <li class = "tags"><a href="#">#${photographersTags}</a></li>`
+            })
+        }
     }
+    articles();
+
 }
 getPictures();
 
+const getTags = async () => {
+    let responseTags = await fetch('https://cors-anywhere.herokuapp.com/https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeData.json')
+    let tagsLinks = await responseTags.json();
+    const tagPortrait = document.getElementById('portrait');
 
 
+    let listTags = tagsLinks.photographers;
+    console.log(listTags);
+
+/**************************************************************************************************************************************************************/
+    /*function monfiltre (element, index, self){
+        console.log(element);
+        console.log(index);
+        console.log(self);
+    }
+
+    monfiltre();*/
+
+    
+    /*premiere methode pour filtrer les #portrait
+    let listTagPortrait = listTags.filter(function (element) {
+        for (let i = 0; i < element.tags.length; i++) {
+            if (element.tags[i] === 'portrait') {
+                return element;
+            }
+        }
+    });
+
+    console.log(listTagPortrait);
+
+
+/**************************************************************************************************************************************************************/
+
+
+ 
+
+
+
+
+}
+
+getTags();
 
 
 
