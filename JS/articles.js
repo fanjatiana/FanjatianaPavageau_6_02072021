a530d214099dcc0c0e19838ddbce82a29c6ba33b
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2021-07-13 23:11:27 +0200

    finitions de la partie liens de navigation en js

--- JS/articles.js
+++ JS/articles.js
@@ -3,15 +3,16 @@ const article = document.querySelector('article');
 const container = document.getElementById('container');
 
 
-/*************************************************************************** */
+/**********************************************Requête pour l'affichage de la liste de photographes********************************************************/
 
+/*chargement du fichier Json avec l'URL*/
 /*fetch('https://cors-anywhere.herokuapp.com/https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeData.json')
     .then(response => response.json())
     .then(pictures => console.log(pictures))*/
 
 
 
-/*chargement du fichier Json avec l'URL*/
+/*chargement du fichier Json en locale*/
 const getPictures = async () => {
     let response = await fetch("./JS/data.json")
     let data = await response.json();
@@ -81,7 +82,7 @@ const getPictures = async () => {
 getPictures();
 
 
-/* Requête liens tags*/
+/****************************************** Requête pour la partie navigation***************************************/
 
 
 
@@ -94,41 +95,53 @@ const getTags = async () => {
     };
     err2;
 
+
+    /*création d'une variable contenant la liste des photographes*/
     let listOfPhotographers = tagsLinks.photographers;
 
-    console.log(listOfPhotographers);
-
-    const tagPortrait = document.getElementById('portrait');
+    /*on recherche dans le DOM la balise NAV contenant la classe nav-tag-links pour pouvoir plus tard y afficher la liste des liens de navigation*/
     const nav = document.querySelector('.nav-tag-links');
 
-    let personList = 0;
+    /*let personList = 0;
     for (let i = 0; i < tagsLinks.photographers.length; i++) {
         personList++;
         console.log(personList);
-    }
+    }*/
 
-
-   let tagsArray = [];
+    /*on cré un  tableau vide et on utilise des boucles pour rechercher et recupérer toute la liste des tags qu'on va ajouter dans ce tableau*/ 
+    let tagsArray = [];
     for (let tags of listOfPhotographers) {
-        
+
         let arrayList = tags.tags;
-        for(let tagName of arrayList){
+        for (let tagName of arrayList) {
             console.log(tagName);
             tagsArray.push(tagName);
         }
-        /*let array1 = tags.tags[0];
-        let array2 = tags.tags[1];
-        let array3 = tags.tags[2];
-        let array4 = tags.tags[3];
+    };
+   
+    
+    /*on supprimer les tags doublons du tableau*/
+    let uniqueSet = new Set(tagsArray);
+    let backToArrayTags = [...uniqueSet];
+    console.log(backToArrayTags);
+
+
+    /*on affiche le nouveau tableau des tags en tant que liens de navigation dans le html. Pour celà on cré une boucle for*/
+   
+    const navUl = document.querySelector(".links-list");
+    
+    for (let elements of backToArrayTags ){
+        let tagsTheme = elements;
+
+        navUl.innerHTML += `
+        
+                <li><a href=""># ${tagsTheme}</a></li>
+            `
+        }
 
         
-        tagsArray.push(array1,array2,);*/
-
+        
        
-    };
-
-   
-    console.log(tagsArray);
 
 
 
@@ -137,7 +150,11 @@ const getTags = async () => {
 
 
 
-    nav.innerHTML = `<ul id="tag">
+
+
+
+
+    /*nav.innerHTML = `<ul id="tag">
     <li><a id ="portrait" href="#">#Portrait</a></li>
     <li><a href="">#Art</a></li>
     <li><a href="">#Fashion</a></li>
@@ -146,7 +163,7 @@ const getTags = async () => {
     <li><a href="">#Sport</a></li>
     <li><a href="">#Animals</a></li>
     <li><a href="">#Events</a></li>
-</ul>`;
+</ul>`;*/
 
 
 
