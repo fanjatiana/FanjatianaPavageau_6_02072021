b3fd534437e32da05494b8b32591e177139a4187
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2021-07-14 19:23:44 +0200

    modifications code js

--- JS/articles.js
+++ JS/articles.js
@@ -26,104 +26,36 @@ const getPictures = async () => {
     /* on cré une variable pour réccupérer la liste des photographes du json*/
     let photographersList = data.photographers;
 
-    /*on cré et initialise une variable pour le nombre de liens tag des photographes*/
-    let numbTags = 0;
-
-
     /*boucle for pour ajouter les articles des photographes*/
     for (let element of photographersList) {
-
-        /* création de variables pour recupérer les données json*/
-        let portrait = element.portrait;
-        let name = element.name;
-        let city = element.city;
-        let price = element.price;
-        let country = element.country;
-        let tagline = element.tagline;
-        let tags = element.tags;
-
-        /*on incrémente la variable pour recupérer le nombre de tags des photographes*/
-        numbTags++;
 
         /*on cré un bloc de code dans le html qui affichera les photos et infos des photographes*/
         container.innerHTML +=
             `<article>
            <section class="id-photograph">
                <a href="#">
-                   <img src="/Photos/gallery/Photographers-Photos/${portrait}">
-                   <h2>${name}</h2>
+                   <img src="/Photos/gallery/Photographers-Photos/${element.portrait}">
+                   <h2>${element.name}</h2>
                </a>
            </section>
            <section class="infos-photograph">
-               <p>${country}, ${city}</p>
-               <p>${tagline}</p>
-               <p>${price}€/jour</p>
+               <p>${element.country}, ${element.city}</p>
+               <p>${element.tagline}</p>
+               <p>${element.price}€/jour</p>
            </section>
            <section class="tag-links">
-               <ul id="tagList${numbTags}"></ul>
+               <ul id="tagList${element.tags}"></ul>
            </section>
        </article>`;
 
         /*on effectue une boucle forEach pour ajouter les liens des tags associés aux photographes*/
-        let tagList = document.getElementById("tagList" + numbTags);
-        [...tags].forEach(photographersTags => {
+        let tagList = document.getElementById("tagList" + element.tags);
+        [...element.tags].forEach(photographersTags => {
             tagList.innerHTML +=
                 ` <li class = "tags" "><a class = "links" href="#">#${photographersTags}</a></li>`
         })
-
     }
-
-  
-    let listTags = data.photographers;
-
-  
-  function filterOfPhotographers () {
-    container.innerHTML = ""; 
-  listTags.filter(function (element) {
-    
-        for (let i = 0; i < element.tags.length; i++) {
-            container.innerHTML += " ";
-            if (element.tags[i] === 'events') {
-                
-                console.log(element)
-               
-
-                let photographName = element.name;
-                let photographCity = element.city;
-                let photographCountry = element.country;
-                let photographPrice = element.price;
-                let photographTagline = element.tagline;
-                let photographPortrait = element.portrait;
-
-                container.innerHTML +=
-            `<article>
-           <section class="id-photograph">
-               <a href="#">
-                   <img src="/Photos/gallery/Photographers-Photos/${photographPortrait}">
-                   <h2>${photographName}</h2>
-               </a>
-           </section>
-           <section class="infos-photograph">
-               <p>${photographCountry}, ${photographCity} </p>
-               <p>${photographTagline}</p>
-               <p>${photographPrice}€/jour</p>
-           </section>
-           <section class="tag-links">
-               <ul id="tagList${numbTags}"></ul>
-           </section>
-       </article>`
-                
-            } 
-        }
-    }); 
-  }
-
-  filterOfPhotographers();
-    
-
-   
-
-}  
+}
 
 getPictures();
 
@@ -171,44 +103,60 @@ const getTags = async () => {
 
         navUl.innerHTML += `
         
-                <li><a class = "links" href=""># ${tagsTheme}</a></li>
-            `
+                <li><a class = "links" href=""># ${tagsTheme}</a></li>    `
     }
-
-    /*on applique le systeme de filtre*/
-
- 
+}
+getTags();
 
 
+/***********************************************************Requête Filtre **********************************************************************/
 
-    /**************************************************************************************************************************************************************/
-    /*function monfiltre (element, index, self){
-        console.log(element);
-        console.log(index);
-        console.log(self);
-    }
+const getFilter = async () => {
+    let responsefiltre = await fetch("./JS/data.json")
+    let dataLinks = await responsefiltre.json();
+    let err = function (err) {
+        // Une erreur est survenue
+        alert(err);
+    };
+    err;
 
-    monfiltre();*/
+    let listTags = dataLinks.photographers;
+    /*on cré et initialise une variable pour le nombre de liens tag des photographes*/
 
+    function filterOfPhotographers() {
+        container.innerHTML = "";
+        listTags.filter(function (element) {
 
-    /*premiere methode pour filtrer les #portrait
-    let listTagPortrait = listTags.filter(function (element) {
-        for (let i = 0; i < element.tags.length; i++) {
-            if (element.tags[i] === 'portrait') {
-                return element;
+            if (element.tags.includes('portrait')) {
+                let photographTags = element.tags;
+                console.log(photographTags)
+
+                container.innerHTML +=
+                    `<article>
+               <section class="id-photograph">
+                   <a href="#">
+                       <img src="/Photos/gallery/Photographers-Photos/${element.portrait}">
+                       <h2>${element.name}</h2>
+                   </a>
+               </section>
+               <section class="infos-photograph">
+                   <p>${element.country}, ${element.city} </p>
+                   <p>${element.tagline}</p>
+                   <p>${element.price}€/jour</p>
+               </section>
+               <section class="tag-links">
+                   <ul id="tagList">${element.tags}</ul>
+               </section>
+           </article>`
             }
-        }
-    });
+        });
+    }
 
-    console.log(listTagPortrait);
-
-
-/**************************************************************************************************************************************************************/
+    filterOfPhotographers();
 
 }
 
-getTags();
-
+getFilter();
 
 
 
