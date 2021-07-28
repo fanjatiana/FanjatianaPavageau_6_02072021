0bd2430a76ebdcbd9c3816e4feb6b249ecb66452
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2021-07-29 00:40:17 +0200

    affichage des photos du photographe Nikolic

--- JS/page-photographers.js
+++ JS/page-photographers.js
@@ -6,30 +6,30 @@ const idPhotograph = async () => {
         alert(err);
     };
     err;
-  
+
     //on recupère la nodeliste des photographes dans le Json
     let photographersList = data.photographers;
 
     //on recupère dans le DOM l'Id de la section show-id-photograph 
     const showPhotograph = document.getElementById("show-id-photograph");
 
-   
+
     //on recherche l id dans l url
     var getUrl_id = window.location.search;
-  
+
     // on extrait l id
     let getId = getUrl_id.slice(4);
 
     //on modifie type de GetId string--> number
     const newGetId = Number(getId);
-    
+
     // on va rechercher dans le Json l'id correspondant à la variable newGetId avec la methode find
     const photographSelected = photographersList.find((element) => element.id === newGetId);
-    console.log(photographSelected)
+
 
     //on ajoute le contenu html avec les données de chaques photographes correspondant à l Id de l'url
-    showPhotograph.innerHTML += 
-    `<div id = "presentation ${photographSelected.id}">
+    showPhotograph.innerHTML +=
+        `<div id = "presentation ${photographSelected.id}">
         <h1>${photographSelected.name}</h1>
         <p>${photographSelected.city}, ${photographSelected.country}</p>
         <p>${photographSelected.tagline}</p>
@@ -42,7 +42,7 @@ const idPhotograph = async () => {
     let showTagsList = document.getElementById("tagsList" + photographSelected.tags);
     photographSelected.tags.forEach(allTags => {
         showTagsList.innerHTML +=
-            `<li class ="tags"><a class = "links" href="#"> #${allTags}</a></li>`           
+            `<li class ="tags"><a class = "links" href="#"> #${allTags}</a></li>`
     })
 
     const titlePagePhotograph = document.getElementById("photograph-name");
@@ -63,16 +63,16 @@ const filtrePhotographersWorks = async () => {
     err;
 
 
-     //on recupère la nodeliste des photographes dans le Json
-     let photographersList = data.photographers;
+    //on recupère la nodeliste des photographes dans le Json
+    let photographersList = data.photographers;
 
-     //on recupère dans le DOM l'Id de la section filter-sort-by
+    //on recupère dans le DOM l'Id de la section filter-sort-by
     const worksFilter = document.getElementById("filter-sort-by");
-    console.log(worksFilter);
 
 
-    worksFilter.innerHTML += 
-    `<h2>Trier par</h2>
+
+    worksFilter.innerHTML +=
+        `<h2>Trier par</h2>
     <ul id="list-filter">
         <li><a href="">Popularité</a></li>
         <li><a href="">Date</a></li>
@@ -94,44 +94,174 @@ const photographersWorks = async () => {
     };
     err;
 
-     //on recupère la nodeliste des photographes dans le Json
-         //on recupère la nodeliste des photographes dans le Json
+
+    //on recupere la liste des photographes dans le data
     let photographersList = data.photographers;
-     let photographersMedia = data.media;
-     console.log(photographersMedia)
-    
 
-     //on recupère dans le DOM l'Id de la section filter-sort-by
+    //on recupère la nodeliste des medias des photographes dans le Json
+    let photographersMedia = data.media;
+    console.log(photographersMedia);
+
+    //on recupère dans le DOM l'Id de la section works-list
     const works = document.getElementById("works-list");
-    console.log(works);
 
 
-      //on recherche l id dans l url
-      var getUrl_id = window.location.search;
+
+
+
+
+    /***************************************************************************pour recuperer l id des photographes*************************************/
+
+    //on recherche l id dans l url
+    var getUrl_id = window.location.search;
+
+    // on extrait l id
+    let getId = getUrl_id.slice(4);
+
+    //on modifie type de GetId string--> number
+    const newGetId = Number(getId);
   
-      // on extrait l id
-      let getId = getUrl_id.slice(4);
-     
-  
+
+
+/************************************************************************************ajout des articles******************************************************** */
    
-  
-      //on modifie type de GetId string--> number
-      const newGetId = Number(getId);
+        
+          
+            for ( let namePhotograph of photographersList){
+                
+                let name = namePhotograph.name;
+                let nameCut = name.split(' ');
+                lastName = nameCut[1];
+                console.log(lastName)
+                   
+            } 
+         
+            for(let element of photographersMedia){
+            if(element.photographerId === newGetId && element.image){
+                console.log(element)
+                works.innerHTML +=
+                `<article id ="artistArticle ${element.photographerId}" class = "pictures video">
+                <img class="picturesList" src = "./Photos/gallery/${lastName}/${element.image}">
+                <h3>${element.title}</h3>
+                <p>${element.likes}</p>
+             </article>`
+
+           
+        }
+
+       
+
+    }
+
+    for(let e of photographersMedia){
+        
+        if(e.photographerId === newGetId && e.video){
+            works.innerHTML +=
+        `<article id ="artistArticle ${e.photographerId}" class = "pictures video">
+                <video controls width="500">
+            <source src="./Photos/gallery/${lastName}/${e.video}" type="video/mp4">
+        </video>
+                <h3>${e.title}</h3>
+                <p>${e.likes}</p>
+             </article>`
+    }
+}
+   
+
+
+
+
+/*photographersMedia.forEach(element => {
+
+
+        if (element.photographerId === newGetId) {
+            console.log(element)
+            works.innerHTML +=
+            `<article id ="${element.photographerId}" class = "pictures">
+            
+            <h3>${element.title}</h3>
+            <p>${element.likes}</p>
+         </article>`
+
+        }
+        
+        
+    });$*/
+
+
+
+
+   
+
+
+/***********************************************************************ajout des images************************************************************************ */
+    /*for (i = 0; i < photographersList.length; i++) {
+        const pictures = document.querySelector(".pictures");
+        console.log(pictures)
+        let name = photographersList[i].name;
+        let nameCut = name.split(' ');
+        let lastName = nameCut[1];
+
+        photographersMedia.forEach(element => {
+            if (element.photographerId === newGetId) {
+                pictures.innerHTML +=
+                    `<img class="picturesList" src = "./Photos/gallery/${lastName}/${element.image}">`
+            }
+
+
+        })
+
+    }  */
+};
+
+photographersWorks();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/****************************************************************************************************************************************************** */
+
+
+    /*boucle pour récupérer tous les noms des photographes;
 
     
-    const mediaSelected= photographersMedia.find((element) => element.photographerId === newGetId);
-    console.log(mediaSelected.video);
+    for (i = 0; i < photographersList.length; i++) {
+        let name = photographersList[i].name;
+        let nameCut = name.split(' ');
+        let lastName=nameCut[1];
+        console.log(lastName)
 
-   
-
-    works.innerHTML += 
-    `<article id ="${mediaSelected.photographerId}">
+        /*works.innerHTML +=
+        `<article id ="${mediaSelected.photographerId}" class = "video">
         <video controls width="500">
-            <source src="./Photos/gallery/Nabeel/${mediaSelected.video}" type="video/mp4">
+            <source src="./Photos/gallery/${lastName}/${mediaSelected.video}" type="video/mp4">
         </video>
         <h3>${mediaSelected.title}</h3>
         <p>${mediaSelected.likes}</p>
-    </article>`
-}
+     </article>`*/
 
-photographersWorks();
+
+ /*works.innerHTML +=
+             `<article id ="${mediaSelected.photographerId}" class = "video">
+             <video controls width="500">
+                 <source src="./Photos/gallery/${lastName}/${mediaSelected.video}" type="video/mp4">
+             </video>
+             <h3>${mediaSelected.title}</h3>
+             <p>${mediaSelected.likes}</p>
+          </article>`*/
